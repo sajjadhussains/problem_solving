a6b59a1b67c212c3bf466b77395df702eb6405b1
sajjadhussains <shuvosajjad70@gmail.com>
2024-11-02 00:23:47 +0600

    after solving 1 problem

--- contest/1_November_2024/first.cpp
+++ contest/1_November_2024/first.cpp
@@ -7,7 +7,32 @@ int main()
     int t;
     cin>>t;
     while(t--){
-        
+        int n;
+        cin>>n;
+        int sz=2*n;
+        int arr[sz];
+        int ones=0;
+        int zeros=0;
+        for(int i=0;i<sz;i++){
+            cin>>arr[i];
+            if(arr[i]==0){
+                zeros++;
+            }
+            else{
+                ones++;
+            }
+        }
+        if(zeros==0){
+            cout<<"0 0"<<endl;
+        }
+        else{
+            if(zeros>=ones){
+                cout<<(zeros%2)<<" "<<ones<<endl;
+            }
+            else{
+                cout<<ones%2<<" "<<zeros<<endl;
+            }
+        }
     }
     return 0;
 }
